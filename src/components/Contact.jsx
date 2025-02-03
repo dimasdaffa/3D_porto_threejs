b0f77ddd5c1dc2fa23887391d634892c9f14ed66
dimasdaffa <dimas.de2004@gmail.com>
2025-02-03 13:23:03 +0700

    add contact logic

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,16 +33,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_levbcxy',
+        'template_jlkpzjb',
         {
           from_name: form.name,
-          to_name: "Wannabe Developer",
+          to_name: "Dimas Daffa",
           from_email: form.email,
           to_email: "dimas.de2004@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'SVAtv2h-A7eFSt1Bo'
       )
       .then(
         () => {
