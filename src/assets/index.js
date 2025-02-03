8d7e813226365c41137d536313cf53da24138e07
dimasdaffa <dimas.de2004@gmail.com>
2025-02-03 11:59:12 +0700

    work section

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,8 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import doscom from "./company/doscom.png";
+import kampus from "./company/kampus.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +50,8 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  doscom,
+  kampus,
   carrent,
   jobit,
   tripguide,
